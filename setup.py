d740374d695da8507e881bd2bcacae8d19f46b4d
Chris Hamill <chamill@ets.org>
2022-06-02 01:02:17 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from setuptools import find_packages, setup
+
+PROJECT_ROOT = Path(__file__).resolve().parent
+
+setup(
+    name='nkml',
+    version='1.0.0',
+    description='Resources for building fake North Korean propaganda generators',
+    long_description=(PROJECT_ROOT / 'README.md').read_text(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/cohml/nklm',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 3.10'
+    ],
+    entry_points={
+        'console_scripts':
+            [
+                'scrape = nklm.collect.scrape:main',
+            ]
+        },
+)
